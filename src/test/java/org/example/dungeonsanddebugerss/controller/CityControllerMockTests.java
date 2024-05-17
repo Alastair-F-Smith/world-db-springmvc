ff0cfeb48a03b38e82b00bbaecb487f782628509
Alastair Smith <alithton@proton.me>
2024-05-17 14:54:32 +0100

    Implement unit tests for the delete method in city controller using MockMvc

--- src/test/java/org/example/dungeonsanddebugerss/controller/CityControllerMockTests.java
+++ src/test/java/org/example/dungeonsanddebugerss/controller/CityControllerMockTests.java
@@ -2,19 +2,13 @@ package org.example.dungeonsanddebugerss.controller;
 
 import org.example.dungeonsanddebugerss.controllers.CityController;
 import org.example.dungeonsanddebugerss.model.entities.CityEntity;
-import org.example.dungeonsanddebugerss.model.exception.KeyNotFoundException;
 import org.example.dungeonsanddebugerss.service.CityService;
 import org.example.dungeonsanddebugerss.service.CountryService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -22,6 +16,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @WebMvcTest(CityController.class)
 public class CityControllerMockTests {
@@ -47,17 +42,11 @@ public class CityControllerMockTests {
 
         mockMvc.perform(delete("/api/city/" + cityId).param("key", "test"))
                .andExpect(status().isOk());
-
-//        webTestClient.delete()
-//                     .uri("http://localhost:8080/api/city/" + cityId + "?key=test")
-//                     .exchange()
-//                .expectStatus()
-//                .is2xxSuccessful();
     }
 
     @Test
     @DisplayName("Given a delete request for a city id that is in the database, the response body contains a city with that id")
-    void givenADeleteRequestForACityIdThatIsInTheDatabaseTheResponseBodyContainsACityWithThatId() {
+    void givenADeleteRequestForACityIdThatIsInTheDatabaseTheResponseBodyContainsACityWithThatId() throws Exception {
         int cityId = 100;
         CityEntity returnedCity = new CityEntity();
         returnedCity.setId(cityId);
@@ -65,29 +54,21 @@ public class CityControllerMockTests {
         when(cityService.getCityById(anyInt()))
                 .thenReturn(Optional.of(returnedCity));
 
-//        mockMvc.perform(delete("/api/city/" + cityId).param("key", "test"))
-//                .andExpect(jsonPath("$.id", is()));
-
-//        webTestClient.delete()
-//                     .uri("http://localhost:8080/api/city/" + cityId + "?key=test")
-//                     .exchange()
-//                     .expectBody(CityEntity.class)
-//                .value(city -> Assertions.assertEquals(cityId, city.getId()));
+        mockMvc.perform(delete("/api/city/" + cityId).param("key", "test"))
+                .andExpect(jsonPath("$.id", is(cityId)));
     }
 
     @Test
     @DisplayName("Given a delete request without an API key, a 401 response code should be returned")
-    void givenADeleteRequestWithoutAnApiKeyA403ResponseCodeShouldBeReturned() {
+    void givenADeleteRequestWithoutAnApiKeyA403ResponseCodeShouldBeReturned() throws Exception {
         int cityId = 100;
         CityEntity returnedCity = new CityEntity();
         returnedCity.setId(cityId);
         when(cityService.getCityById(anyInt()))
                 .thenReturn(Optional.of(returnedCity));
-//        webTestClient.delete()
-//                     .uri("http://localhost:8080/api/city/" + cityId)
-//                     .exchange()
-//                .expectStatus()
-//                .isUnauthorized();
+
+        mockMvc.perform(delete("/api/city/" + cityId).param("key", ""))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
@@ -95,7 +76,22 @@ public class CityControllerMockTests {
     void givenADeleteRequestForACityIdThatDoesNotExistReturnsA400ResponseCode() throws Exception {
         int cityId = 9999;
 
-        mockMvc.perform(delete("/api/city/" + cityId))
-                .andExpect(status().isUnauthorized());
+        when(cityService.getCityById(cityId))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/city/" + cityId).param("key", "test"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Given a delete request for a city id that does not exist, the response has the correct message")
+    void givenADeleteRequestForACityIdThatDoesNotExistTheResponseHasTheCorrectMessage() throws Exception {
+        int cityId = 9999;
+
+        when(cityService.getCityById(cityId))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(delete("/api/city/" + cityId).param("key", "test"))
+               .andExpect(jsonPath("$.message", is("Could not find city: " + cityId)));
     }
 }
